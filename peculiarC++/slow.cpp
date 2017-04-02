82f05648a2da28abfa594d4b4c7a46ca172037cc
LucaPaterlini <LucaPaterlini@github.com>
2017-04-02 12:52:02 +0100

    first commit

---
+++ peculiarC++/slow.cpp
@@ -0,0 +1,20 @@
+#include <algorithm>
+#include <ctime>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    const unsigned arraySize = 32768;int data[arraySize];
+    for (unsigned c = 0; c < arraySize; ++c)data[c] = rand() % 256;
+    sort(data, data + arraySize);
+    clock_t start = clock();
+    long long sum = 0;
+    for (unsigned i = 0; i < 100000; ++i) {
+        for (unsigned c = 0; c < arraySize; ++c) { if (data[c] >= 128)sum += data[c];
+        }
+    }
+    double elapsedTime = static_cast<double>(clock() - start) / CLOCKS_PER_SEC;
+    cout << elapsedTime << endl;
+    cout << "sum = " << sum << endl;
+}
